334af392b9228c86c2277720de3cb3f48c8635c0
Azucarrrr <priyangshu10diablo@gmail.com>
2024-01-26 10:03:55 +0530

    logout functionality add

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -5,6 +5,7 @@ export default function UserProfile({params}: any){
             <hr />
             <p className="text-4xl">Profile Page 
             <span className="p-2 ml-2 rounded bg-orange-500 text-black">{params.id}</span></p>
+            <hr />
         </div>
     )
 }
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,15 +1,29 @@
 "use client";
 import Link from "next/link";
 import React, { useState } from "react";
-import useRouter from "next/navigation";
+import {useRouter} from "next/navigation";
 import axios from "axios";
+import toast from "react-hot-toast";
 
 export default function ProfilePage(){
+    const router = useRouter()
+    const logout = () => {
+        try {
+            axios.get('/api/users/logout')
+            toast.success('Logout successful')
+            router.push('/login')
+        } 
+        catch (error:any) {
+            console.log(error.message);
+            toast.error(error.message)
+        }
+    }
     return(
         <div className="flex flex-col items-center justify-center min-h-screen py-2">
             <h1>Profile</h1>
             <hr />
-            <h1>Profile Page</h1>
+            <h1>Profile Page</h1>      
+            <button onClick={logout} className="py-2 px-4 rounded font-bold text-white bg-blue-400 hover:bg-blue-700">Logout</button>
         </div>
     )
 }
